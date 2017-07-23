6ac984820a589f35effd09fea451a11dfe66ad96
Zev Isert <zev.isert@gmail.com>
2017-07-23 13:56:24 -0700

    Implemented Montgomery Modular Multiplication
    Could help to prescale result before calling

--- RSA/rsa.c
+++ RSA/rsa.c
@@ -35,16 +35,19 @@ typedef struct
 int encrypt(int payload, key publkey);
 int decrypt(int payload, key privkey);
 
+unsigned int bitlen(unsigned long long int x);
+unsigned int mmm(unsigned int X, unsigned int Y, unsigned int M);
+
+#define BITTST(X, i) ((X & i) != 0)
 
 int main(void)
 { 
 	int input = 123;
 	key publkey = { publ_exponent, modulus };
 	key privkey = { priv_exponent, modulus };
-	int output = encrypt(input, publkey);
+	int output = mmm(17, 22, 23);
 	printf("%d\n", output);
-	output = decrypt(output, privkey);
-	printf("%d\n", output);
+
 	exit(0);
 } 
 
@@ -70,4 +73,48 @@ int decrypt(int payload, key privkey)
 		out *= payload;
 	}
 	return out % privkey.modulus;
+}
+
+unsigned int bitlen(unsigned long long int x)
+{
+	unsigned int bits = 0;
+	unsigned long long int val = x;
+	for (; val != 0; ++bits) val >>= 1;
+	return bits;
+}
+
+unsigned int mmm(unsigned int X, unsigned int Y, unsigned int M)
+{
+	unsigned int m = bitlen(M);
+	unsigned int R = 1 << m;
+
+	unsigned int T = 0;
+	unsigned register int i = 1;
+	unsigned register int nu;
+
+	#ifdef VERBOSE
+	unsigned register int j = 0;
+	#endif // VERBOSE
+
+	for (; i < R; i <<= 1)
+	{
+		/* nu = T(0) OR (X(i) AND Y(0)) where B(n) is the nth bit of B from the right */
+		nu = BITTST(T, 0x1) | (BITTST(X, i) & BITTST(Y, 0x1));
+	
+		/* T = ( T + X(i) * Y + nu * M ) / 2 */
+		T = (T + BITTST(X, i) * Y + nu * M) >> 1;
+
+		#ifdef VERBOSE
+		j += 1;
+		printf("i: %d, X(i): %d, nu: %d, T: %d\n", j, BITTST(X, i), nu, T);			  
+		#endif // VERBOSE
+
+	}
+	
+	if (T >= M)
+	{
+		T -= M;
+	}
+
+	return T;
 }
